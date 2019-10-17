8ea298347c583fdd09810750631b70fb25d47afa
Benjamin Parkka <Bparkka@comcast.net>
2019-10-16 22:08:38 -0400

    ProblemSet5

--- ProblemSet5_2.c
+++ ProblemSet5_2.c
@@ -1,1 +1,8 @@
+#include<stdio.h>
 
+void main()
+{
+    int arr[100], n[100], i, temp;
+    printf("Enter the array");
+    for(i=0; i<=n-1)
+}
--- ProblemSet5_4.c
+++ ProblemSet5_4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+enum scale{
+    Ionian, Dorian, Phrygian, Lydian, Mixolidian, Aeolian, Locrian
+};
+Ionian = {0, 2, 4, 5, 7, 9, 11, 12};
+Dorian = {0, 2, 3, 5, 7, 9, 10, 12};
+Phrigian = {0, 1, 3, 5, 7, 8, 10, 12};
+Lydian = {0, 2, 4, 6, 7, 9, 11, 12};
+Mixolydian = {0, 2, 4, 5, 7, 9, 10, 12};
+Aeolian = {0, 2, 3, 5, 7, 8, }
+
+struct Scale {
+    unsigned char str[];
+
+
+    };
+typedef struct Scale Scale;
+
+
+int main(){
+
+}
